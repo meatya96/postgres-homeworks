ad3bc7dea265fbb16fda580a644470c856508182
Meatya <meatya96@gmail.com>
2024-04-16 13:42:18 +0600

    initial commit

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,23 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import pandas as pd
+def opening_naruto(value):
+
+    df = pd.read_csv(value)
+    values_list = df.values.tolist()
+    return values_list
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='89088800080'
+)
+
+cur = conn.cursor()
+
+print(opening_naruto('north_data/employees_data.csv'))
+cur.executemany("INSERT INTO customers VALUES (%s, %s, %s)", opening_naruto('north_data/customers_data.csv'))
+cur.executemany("INSERT INTO employees VALUES (%i, %s, %s, %s, %d, %s)", opening_naruto('north_data/employees_data.csv'))
+cur.executemany("INSERT INTO orders VALUES (%i, %s, %i, %d, %s)", opening_naruto('north_data/orders_data.csv'))
+
+conn.commit()
